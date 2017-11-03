e6c21447e0d31293611a91d3da76e2c3c36eff3a
bhagath <bhagath@getcustomexposure.com>
2017-11-04 01:05:59 +0530

    added new edit create update actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,30 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+		@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+		if @portfolio_item.save
+			redirect_to portfolios_path, notice: 'Your portfolio item is now live.' 
+		else
+			render :new 
+		end
+	end
+
+	def edit
+		@portfolio_item = Portfolio.find(params[:id]) 
+	end
+
+	def update
+		@portfolio_item = Portfolio.find(params[:id]) 
+		if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+			redirect_to portfolios_path, notice: 'Your portfolio item is now live.' 
+		else
+			render :edit 
+		end
+	end
 end
