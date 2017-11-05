43994078cb022a1d84c2297352f7d335825310d6
bhagath <bhagath@getcustomexposure.com>
2017-11-05 16:21:24 +0530

    adding guest user account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	
 	include DeviseWhitelist
-	
+
 	before_action :set_source
 	include SetSource
+	include CurrentUserConcern
 
 end
