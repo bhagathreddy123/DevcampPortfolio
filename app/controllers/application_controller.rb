09f5f8ebeb06496116f3c012b89b459dcad50234
bhagath <bhagath@getcustomexposure.com>
2017-11-06 09:55:06 +0530

    strategy for building custom ruby gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,23 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	
 	include DeviseWhitelist
-
-	before_action :set_source
 	include SetSource
 	include CurrentUserConcern
 	include DefaultPageContent
+	before_action :set_copyright
+	def set_copyright
+		@copyright = CustomViewTool::Render.copyright 'bhagath', 'All rights reserved'
+	end
 
+
+end
+
+
+module CustomViewTool
+	class Render
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+
+		end
+	end
 end
